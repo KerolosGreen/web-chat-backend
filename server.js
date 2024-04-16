8004054ed5ad7ec48de23b65f2ab51c452fb7272
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-04-16 02:34:18 +0200

    Updating CORS Address

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(3005,{
     cors:{
-        origin:['https://web-chat-client-three.vercel.app']
+        origin:['https://kerolos-web-chat.vercel.app/']
         
     }
 })
